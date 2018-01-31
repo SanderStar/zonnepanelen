6fd67d369c55586125bf9ef1a6f9dd17ae55254e
Sander Star <sanderstar@creetion.com>
2018-01-31 12:28:53 +0100

    Toevoegen extra routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,44 @@
-const express = require("express");
-const path = require('path');
-const router = express.Router();
+const express = require("express")
+const path = require("path")
+const request = require("request")
+
+const router = express.Router()
 
 // Debuggen
 router.use(function(req, res, next) {
-	console.log("Router aangeroepen ", new Date());
-	next();
-});
+	console.log("Router aangeroepen ", new Date())
+	next()
+})
 
 // Show main page
 router.get("/", function(req, res) {
-	res.sendFile(path.resolve("public/index.html"));
-});
+	res.sendFile(path.resolve("public/index.html"))
+})
 
-module.exports = router;
+// TODO Setting timer / remove
+router.get("/timer", function(req, res) {
+	// Reconstruct original URL
+	var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+	console.log("Setting timer " + fullUrl)
+	request(fullUrl, function(error, result, body) {
+		console.log("response " + response)
+		console.log("error " + error) 
+		console.log("result " + result)
+		console.log("body " + body)
+		response.send("hallo")
+	})
+})
+
+// TODO Get solar panel data / replaced by schedule
+router.get("/solarpanel", function(req, res) {
+	console.log("Getting solar panel data")
+	var url = "https://api.enphaseenergy.com/api/v2/systems/1338274/summary?key=97bd72a51ce15f497cef4eb553117b75&user_id=4d5455324d446b774f413d3d0a"
+	request(url, function(error, result, body) {
+		console.log("error " + error) 
+		console.log("result " + result)
+		console.log("body " + body)
+		res.send(body)
+	})
+})
+
+module.exports = router
