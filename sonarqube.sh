286fdfcdbca74ba06366df7c9ad6409c5302379f
Sander Star <sanderstar@creetion.com>
2018-12-21 10:03:28 +0100

    juiste syntax voor negeren node_modules folder tbv sonar

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,1 +1,1 @@
-sonar-scanner.bat -D"sonar.zonnepanelen" -D"sonar.organization=sanderstar-github" -D"sonar.sources=." -D"sonar.exclusions=./node_modules/**/*" -D"sonar.host.url=https://sonarcloud.io" -D"sonar.login=91e02e144dd0b21de7079daa66e9cca8d9e69b23"
+sonar-scanner.bat -D"sonar.projectKey=zonnepanelen" -D"sonar.organization=sanderstar-github" -D"sonar.sources=." -D"sonar.exclusions=node_modules/**/*" -D"sonar.host.url=https://sonarcloud.io" -D"sonar.login=91e02e144dd0b21de7079daa66e9cca8d9e69b23"
